8cc55b2c9fe6dda6afd8f006c93a45795dd2e2b7
le-chartreux <le-chartreux-vert@protonmail.com>
2025-12-29 13:48:09 +0100

    d2p2: use traits

--- src/day_02_p2_lib.rs
+++ src/day_02_p2_lib.rs
@@ -5,39 +5,61 @@ pub type IdRange = RangeInclusive<Id>;
 pub type IdSum = Id;
 
 pub fn get_sum_of_invalid_ids(range: IdRange) -> IdSum {
-    range.filter(|&id| is_id_invalid(id)).sum()
+    range.filter(|&id| id.is_invalid()).sum()
 }
 
-fn is_id_invalid(id: Id) -> bool {
-    let number_of_digits = get_number_of_digits(id);
+trait Invalidity {
+    fn is_invalid(&self) -> bool;
+}
 
-    for n in 1..number_of_digits {
-        // TODO: que sur les multiples
-        if number_of_digits.is_multiple_of(n) {
-            if repeat(get_nth_first_digits(id, n), number_of_digits / n) == id {
+trait CountDigits {
+    fn get_number_of_digits(&self) -> u32;
+}
+
+trait Repeat {
+    fn repeat(&self, times: u32) -> Self;
+}
+
+trait FirstDigits {
+    fn get_nth_first_digits(&self, n: u32) -> Self;
+}
+
+impl Invalidity for Id {
+    fn is_invalid(&self) -> bool {
+        let number_of_digits = self.get_number_of_digits();
+
+        for n in 1..number_of_digits {
+            // TODO: que sur les multiples
+            if number_of_digits.is_multiple_of(n)
+                && self.get_nth_first_digits(n).repeat(number_of_digits / n) == *self
+            {
                 return true;
             }
         }
+        false
     }
-    false
 }
 
-fn get_nth_first_digits(id: Id, n: u32) -> Id {
-    id / (10 as Id).pow(get_number_of_digits(id) - n)
+impl CountDigits for Id {
+    fn get_number_of_digits(&self) -> u32 {
+        self.ilog10() + 1
+    }
 }
 
-fn repeat(id: Id, times: u32) -> Id {
-    let number_of_digits = get_number_of_digits(id);
-    // id * (sum(10^n for n in 0..(number_of_digits*rep) avec un pas de number_of_digits))
-    let multiplicator: Id = (0..(number_of_digits * times))
-        .step_by(number_of_digits as usize)
-        .map(|n| 10_u64.pow(n))
-        .sum();
-    multiplicator * id
+impl Repeat for Id {
+    fn repeat(&self, times: u32) -> Self {
+        let multiplicator: Id = (0..(self.get_number_of_digits() * times))
+            .step_by(self.get_number_of_digits() as usize)
+            .map(|n| 10_u64.pow(n))
+            .sum();
+        multiplicator * self
+    }
 }
 
-fn get_number_of_digits(id: Id) -> u32 {
-    id.ilog10() + 1
+impl FirstDigits for Id {
+    fn get_nth_first_digits(&self, n: u32) -> Id {
+        self / (10 as Id).pow(self.get_number_of_digits() - n)
+    }
 }
 
 #[cfg(test)]
@@ -45,19 +67,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_is_id_invalid_case_invalid() {
+    fn test_is_id_valid_case_invalid() {
         for id in [
             11, 22, 99, 111, 999, 1010, 1188511885, 222222, 446446, 38593859, 565656, 824824824,
             2121212121,
         ] {
-            assert!(is_id_invalid(id))
+            assert!(id.is_invalid());
         }
     }
 
     #[test]
-    fn test_is_id_invalid_case_valid() {
+    fn test_is_id_valid_case_valid() {
         for id in [4, 12, 23, 100, 110, 998, 1011, 118851855, 23456, 44646] {
-            assert!(!is_id_invalid(id));
+            assert!(!id.is_invalid());
         }
     }
 
@@ -72,7 +94,7 @@ mod tests {
             (123456789, 7, 1234567),
         ];
         for (id, number_of_digits, expected) in test_cases {
-            assert_eq!(get_nth_first_digits(id, number_of_digits), expected);
+            assert_eq!(id.get_nth_first_digits(number_of_digits), expected);
         }
     }
 
@@ -80,7 +102,7 @@ mod tests {
     fn test_repeat() {
         let test_cases = [(1, 1, 1), (12, 1, 12), (12, 2, 1212), (123, 3, 123123123)];
         for (id, times, expected) in test_cases {
-            assert_eq!(repeat(id, times), expected);
+            assert_eq!(id.repeat(times), expected);
         }
     }
 
@@ -88,7 +110,7 @@ mod tests {
     fn test_get_number_of_digits() {
         let test_cases = [(3, 1), (37, 2), (481, 3), (1234567, 7)];
         for (id, expected) in test_cases {
-            assert_eq!(get_number_of_digits(id), expected);
+            assert_eq!(id.get_number_of_digits(), expected);
         }
     }
 }
